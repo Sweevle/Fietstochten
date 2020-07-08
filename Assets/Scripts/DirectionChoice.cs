a555652eb96c0883454986f98f5ed65b6e95faae
Sweevle <ifitskie@gmail.com>
2020-07-08 11:49:14 +0200

    the test build
    the state of the project during the direction choice test and the result screen test. both are separate scenes

--- Assets/Scripts/DirectionChoice.cs
+++ Assets/Scripts/DirectionChoice.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
@@ -14,9 +13,11 @@ public class DirectionChoice : MonoBehaviour
     private string triggered = "Not in contact";
     GameObject directionArrow;  
     IEnumerator mL;
-    GameObject cam;
+    Vector3 moveDirection = Vector3.back;
+    Vector3 platePos;
     string vName;
 
+    int videoID = 0;
     public GameObject directionChoiceTile;
     public worldScript world;
     public fadePanel fadeUI;
@@ -24,13 +25,15 @@ public class DirectionChoice : MonoBehaviour
     public Sprite leftArrow;
     public Sprite rightArrow;
     public Sprite defaultArrow;
+    public Material skybox;
 
     // Start is called before the first frame update
     void Start()
     {
         videoPlayer = GameObject.Find("Video Player").GetComponent<VideoPlayer>();
-        cam = GameObject.Find("MainCamera");
         mL = measureLean();
+        skybox.SetInt( "_Rotation" ,  291);
+        // start voice explanation
     }
 
     // Update is called once per frame
@@ -40,135 +43,37 @@ public class DirectionChoice : MonoBehaviour
         if (directionSet == true){
             // fadeUI.Fade();
             world.fadedOut = true;
-            world.elapsedTime = 0;
-
-
-            // based on the current video and the chosen direction set the new video and next spawn time of direction choice
+            // world.elapsedTime = 0;
             if (direction == "Left"){
-                Debug.Log("direction is left and video is: " + vName);
-                switch (vName){
-                    case "1":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "4.mp4");
-                        world.next_spawn_time = 52f;
-                        break;
-                    case "2":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "5.mp4");
-                        world.next_spawn_time = 38f;
-                        break;
-                    case "4":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "13.mp4");
-                        world.next_spawn_time = 111f;
-                        break;
-                    case "5":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "8.mp4");
-                        world.next_spawn_time = 134f;
-                        break;
-                    case "6":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "9.mp4");
-                        world.next_spawn_time = 33f;
-                        break;
-                    case "8":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "2.mp4");
-                        world.next_spawn_time = 90f;
-                        break;
-                    case "9":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "6.mp4");
-                        world.next_spawn_time = 5f;
-                        break;
-                    default:
-                        break;
-                }
+                videoPlayer.url = "file://mnt/sdcard/Videos/dc1/ChoiceLeft.mp4";
+                skybox.SetInt( "_Rotation" ,  276);
                 // fadeUI.Fade();
             }
             else if (direction == "Right"){
-                Debug.Log("direction is right and video is: " + vName);
-                switch (vName){
-                    case "1":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "2.mp4");
-                        world.next_spawn_time = 93f;
-                        break;
-                    case "3":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "9.mp4");
-                        world.next_spawn_time = 33f;
-                        break;
-                    case "4":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "12.mp4");
-                        world.next_spawn_time = 6f;
-                        break;
-                    case "5":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "7.mp4");
-                        world.next_spawn_time = 44f;
-                        break;
-                    case "7":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "5.mp4");
-                        world.next_spawn_time = 38f;
-                        break;
-                    case "8":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "4.mp4");
-                        world.next_spawn_time = 52f;
-                        break;
-                    case "9":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "10.mp4");
-                        world.next_spawn_time = 79f;
-                        break;
-                    case "10":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "3.mp4");
-                        world.next_spawn_time = 111f;
-                        break;
-                    default:
-                        break;
-                }
+                //play video right
+                videoPlayer.url = "file://mnt/sdcard/Videos/dc1/ChoiceRight.mp4";
+                skybox.SetInt( "_Rotation" ,  280);
                 // fadeUI.Fade();
             }
             else{
-                Debug.Log("direction is straight and video is: " + vName);
                 //play video straight
-                switch (vName){
-                    case "1":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "3.mp4");
-                        world.next_spawn_time = 111f;
-                        break;
-                    case "2":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "6.mp4");
-                        world.next_spawn_time = 5f;
-                        break;
-                    case "3":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "7.mp4");
-                        world.next_spawn_time = 44f;
-                        break;
-                    case "6":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "8.mp4");
-                        world.next_spawn_time = 134f;
-                        break;
-                    case "7":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "10.mp4");
-                        world.next_spawn_time = 79f;
-                        break;
-                    case "10":
-                        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "4.mp4");
-                        world.next_spawn_time = 52f;
-                        break;
-                    default:
-                        break;
-                }
+                videoPlayer.url = "file://mnt/sdcard/Videos/dc1/ChoiceStraight.mp4";
+                skybox.SetInt( "_Rotation" ,  265);
                 // fadeUI.Fade();
             };
+
+            
            directionSet = false;
            world.fadedOut = false;
         }
     }
 
     private void OnTriggerEnter(Collider other)
-    {   
-        
+    {
         Debug.Log(triggered = "In contact");
-        
-        // only allow the plate under the arrow
         world.shouldMove = false;
         directionArrow = GameObject.Find("Arrow");
-
-        // check video name and start measuring
-        updateVideoName();
+        // updateVideoName();
         StartCoroutine(mL);
     }
 
@@ -176,141 +81,31 @@ public class DirectionChoice : MonoBehaviour
     {
         
         Debug.Log(triggered = "No longer in contact and direction is " + direction);
-        
-        // stop measuring
+    
         StopCoroutine(mL);
-        
-        // remove the direction choice object
         directionChoiceTile = GameObject.Find("directionChoiceTile");
         Destroy(directionChoiceTile);
-
-        // set the bools back to moving
         world.shouldMove = true;
-
-        // direction has been chosen
         directionSet = true;
     }
 
     IEnumerator measureLean(){
         measuring = true; 
-        var f1 = 0.2f;
-        var f2 = -0.2f;
+        var controller = VZPlayer.Controller;
+        var f1 = 0.2;
+        var f2 = -0.2;
+
 
         while ( measuring ){
-            var controller = VZPlayer.Controller;
-
-            //Based on the name of the video it will show the directions it can go and change the arrow accordingly.
-            switch (vName){
-                case "1":
-                    if (controller.HeadRot > f1){
-                        direction = "Left";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = leftArrow;
-                    } else if (controller.HeadRot < f2){
-                        direction = "Right";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = rightArrow;
-                    } else {
-                        direction = "Straight";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = defaultArrow;
-                    }
-                    break;
-                case "2":
-                    if (controller.HeadRot > f1){
-                        direction = "Left";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = leftArrow;
-                    } else {
-                        direction = "Straight";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = defaultArrow;
-                    }
-                    break;
-                case "3":
-                    if (controller.HeadRot < f2){
-                        direction = "Right";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = rightArrow;
-                    } else {
-                        direction = "Straight";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = defaultArrow;
-                    }
-                    break;
-                case "4":
-                    if (controller.HeadRot > f1){
-                        direction = "Left";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = leftArrow;
-                    } else if (controller.HeadRot < f2){
-                        direction = "Right";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = rightArrow;
-                    } else {
-                        if(Random.value<0.5f)
-                            direction = "Right";
-                        else
-                            direction = "Left";
-                    }
-                    break;
-                case "5":
-                    if (controller.HeadRot > f1){
-                        direction = "Left";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = leftArrow;
-                    } else if (controller.HeadRot < f2){
-                        direction = "Right";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = rightArrow;
-                    } else {
-                        if(Random.value<0.5f)
-                            direction = "Right";
-                        else
-                            direction = "Left";
-                    }
-                    break;
-                case "6":
-                    if (controller.HeadRot > f1){
-                        direction = "Left";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = leftArrow;
-                    } else {
-                        direction = "Straight";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = defaultArrow;
-                    }
-                    break;
-                case "7":
-                    if (controller.HeadRot < f2){
-                        direction = "Right";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = rightArrow;
-                    } else {
-                        direction = "Straight";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = defaultArrow;
-                    }
-                    break;
-                case "8":
-                    if (controller.HeadRot > f1){
-                        direction = "Left";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = leftArrow;
-                    } else {
-                        direction = "Straight";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = defaultArrow;
-                    }
-                    break;
-                case "9":
-                    if (controller.HeadRot > f1){
-                        direction = "Left";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = leftArrow;
-                    } else if (controller.HeadRot < f2){
-                        direction = "Right";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = rightArrow;
-                    } else {
-                        if(Random.value<0.5f)
-                            direction = "Right";
-                        else
-                            direction = "Left";
-                    }
-                    break;
-                case "10":
-                    if (controller.HeadRot < f2){
-                        direction = "Right";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = rightArrow;
-                    } else {
-                        direction = "Straight";
-                        directionArrow.GetComponent<SpriteRenderer>().sprite = defaultArrow;
-                    }
-                    break;
-                default:
-                    break;
+            if (controller.HeadRot > f1){
+                direction = "Left";
+                directionArrow.GetComponent<SpriteRenderer>().sprite = leftArrow;
+            } else if ( controller.HeadRot < f2){
+                direction = "Right";
+                directionArrow.GetComponent<SpriteRenderer>().sprite = rightArrow;
+            } else {
+                direction = "Straight";
+                directionArrow.GetComponent<SpriteRenderer>().sprite = defaultArrow;
             }
           yield return null;
         }
@@ -319,15 +114,7 @@ public class DirectionChoice : MonoBehaviour
 
     public void updateVideoName(){
         vName = videoPlayer.url;
-
-        //remove the first part of the file path
-        vName = vName.Replace(Application.streamingAssetsPath, "");
-
-        //if there is a \ in the file path it will be removed -- used on pc
-        vName = vName.Replace("\\", "");
-
-        // if there is a / in the file path it will be removed -- used on oculus go
-        vName = vName.Replace("/", "");
+        vName = vName.Remove(0,25);
         vName = vName.Replace(".mp4", "");
         Debug.Log("videoname is: " + vName);
     }
--- Assets/Scripts/worldScript.cs
+++ Assets/Scripts/worldScript.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -11,7 +10,6 @@ public class worldScript : MonoBehaviour
 
     public float elapsedTime;
     public float globalTime;
-    public float next_spawn_time;
     public bool transitionCanvasActive;
     public bool tutorialDone;
     public bool shouldMove;
@@ -19,80 +17,70 @@ public class worldScript : MonoBehaviour
     
     public GameObject directionChoiceTile;
     public GameObject gameUI;
-    public VideoManager videoManager;
 
     private VideoPlayer videoPlayer;
+    public VideoManager videoManager;
     private AudioSource audioSource;
+    
+    public float next_spawn_time;
+    float first_spawn_time;
+    GameObject transitionCanvas;
     private float maxSpeed;
 
-    GameObject transitionCanvas;
-    float first_spawn_time;
-    
     // Start is called before the first frame update
     void Start()
     {
-        //setting the bools.
-        tutorialDone = false;
-        shouldMove = true;
-        fadedOut = false;
-
-
         transitionCanvas = GameObject.Find("TransitionCanvas");
         transitionCanvasActive = transitionCanvas.activeInHierarchy;
         audioSource = GameObject.Find("Audio Source").GetComponent<AudioSource>();
-        
-        // set starting video.
-        videoPlayer = GameObject.Find("Video Player").GetComponent<VideoPlayer>();
-        videoPlayer.url = Path.Combine(Application.streamingAssetsPath, "1.mp4");
-        videoPlayer.Prepare();
+        tutorialDone = true;
+        shouldMove = true;
+        fadedOut = false;
 
+        // videoPlayer = GameObject.Find("Video Player").GetComponent<VideoPlayer>();
+        // videoPlayer.url = "file://mnt/sdcard/Videos/1.mp4";
+        // videoPlayer.Prepare();
+        //elapsed time + directionchoice time from video
+        // if (tutorialDone == false){
+        //     next_spawn_time = elapsedTime + ((float)videoPlayer.clip.length - audioSource.clip.length - 11f);
+        //     // Debug.Log(next_spawn_time);
+        // } else{
+        //     next_spawn_time = elapsedTime + ((float)videoPlayer.clip.length - 11f);
+        //     // Debug.Log(next_spawn_time);
+        // }
 
-        // check if the tutorial needs to be played, if yes then also take that in account for when the
-        // first spawn time is for the direction choice
-        if (tutorialDone == false){
-            next_spawn_time = elapsedTime + (34f - audioSource.clip.length - 10f);
-            // Debug.Log(next_spawn_time);
-        } else{
-            next_spawn_time = elapsedTime + (34f - 11f);
-            // Debug.Log(next_spawn_time);
-        }
+        next_spawn_time = elapsedTime + 15f;
     }
 
     // Update is called once per frame
     void Update(){
-
         var controller = VZPlayer.Controller;
+
         transitionCanvasActive = transitionCanvas.activeInHierarchy;
-        // if the transition canvas is active don't do anything.
         if (!transitionCanvasActive){
             videoManager.Play();
 
+            // if (elapsedTime > 300f){
 
-            // if the time reaches 5 minutes stop the tour, save the data and go to the result screen
-            if (globalTime > 300f){
+            //     GlobalData.Instance.spins = controller.Spins;
+            //     GlobalData.Instance.speed = maxSpeed;
+            //     GlobalData.Instance.avgSpeed = 5.00f;
+            //     GlobalData.Instance.rpm = (controller.Spins / (elapsedTime / 60));
+            //     GlobalData.Instance.time = globalTime;
+            //     GlobalData.Instance.distance = controller.EstimatedDistance;
 
-                GlobalData.Instance.spins = controller.Spins;
-                GlobalData.Instance.speed = maxSpeed;
-                GlobalData.Instance.avgSpeed = 5.00f;
-                GlobalData.Instance.rpm = (controller.Spins / (elapsedTime / 60));
-                GlobalData.Instance.time = globalTime;
-                GlobalData.Instance.distance = controller.EstimatedDistance;
+            //     SceneManager.LoadScene("resultScreen");
+            // }
 
-                SceneManager.LoadScene("resultScreen");
-            }
-
-
-            // when transitioning to another video pause the video
             if (fadedOut == true){
                 videoManager.Pause();
             }
             
-            // set and update the data shown in the bar during the tour
             if (fadedOut == false){
                 var timeText = gameUI.transform.Find("Time").GetComponent<Text>();
                 elapsedTime += Time.deltaTime;
                 globalTime += Time.deltaTime;
-                timeText.text = globalTime.ToString("f0");
+                timeText.text = elapsedTime.ToString("f0");
 
                 var speed = gameUI.transform.Find("Speed").GetComponent<Text>();
                 var distance = gameUI.transform.Find("Distance").GetComponent<Text>();
@@ -104,9 +92,6 @@ public class worldScript : MonoBehaviour
                 }
             }
 
-
-            // if the elapsed time hits the spawn time check if the tutorial has been done
-            // else skip tutorial
             if (elapsedTime > next_spawn_time){
                 if (tutorialDone == false){
                     playTutorial();
@@ -118,7 +103,6 @@ public class worldScript : MonoBehaviour
         }
     }
 
-    // spawn the object that starts the direction choice
     void spawnDirectionChoiceTile(){
         var t = Instantiate(directionChoiceTile,new Vector3(-2.3f, -5.4f, 31.49f), Quaternion.identity);
         t.name = "directionChoiceTile";
@@ -129,21 +113,17 @@ public class worldScript : MonoBehaviour
         tutorialDone = true;
     }
 
-
-    // play the audio tutorial and after spawn the object.
-    // set the tutorial boolean to true
     void playTutorial(){
+
         audioSource.Play();
         Invoke("spawnDirectionChoiceTile", audioSource.clip.length);
-        next_spawn_time += 60.0f;
+        next_spawn_time += 32.0f;
         tutorialDone = true;
     }
 
-
-    // if the tutorial has been done, spawn object immediately.
     void skipTutorial(){
         spawnDirectionChoiceTile();
         //increment next_spawn_time
-        next_spawn_time += 60.0f;
+        next_spawn_time += 20.0f;
     }
 }
